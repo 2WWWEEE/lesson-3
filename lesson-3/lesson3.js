13a64b52189d5bfcbdeadeb4314a316e0d57779f
2WWWEEE <luckojanova10@gmail.com>
2025-11-30 18:49:42 +0300

    Добавлено пустое содержимое для HTML и JS файлов

---
+++ lesson-3/lesson3.js
@@ -0,0 +1,6 @@
+let titleProject = 'Калькулятор по разработке сайта'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 5000
+let percentage = 30
+let fullPrice = 150000
+let responsive = true   
